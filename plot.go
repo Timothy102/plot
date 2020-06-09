b71cdaeecf5cabc29114cac498bae69ff1b21ba0
Tim Cvetko <52534783+Timothy102@users.noreply.github.com>
2020-06-09 12:11:46 +0200

    Update plot.go

--- plot.go
+++ plot.go
@@ -1,4 +1,4 @@
-//Package plot implements plotting functionality for all sorts of data as well as matrix and vector visualisations. It also provides statistical components of datasets, such as variances and covariances.
+//Package plot implements plotting functionality for all sorts of data as well as matrix and vector visualisations. It also provides statistical components of datasets along with Taylor series approximation of Cosine and the Exponential.
 package plot
 
 import (
