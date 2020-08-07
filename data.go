e356d32988238c1d52309a864c9e57905a48b328
timothy102 <cvetko.tim@gmail.com>
2020-08-07 17:50:39 +0200

    Added the evalution metrics and compressed the repo to data.go and plot.go

--- data.go
+++ data.go
@@ -15,8 +15,8 @@ import (
 
 //Point struct
 type Point struct {
-	x, y   float64
-	isBlue bool
+	x, y float64
+	is   bool
 }
 
 //Points is a slice of Points.
@@ -993,13 +993,13 @@ func DoApproximation(pts Points, file string) error {
 
 //knn is the K-Nearest-Neighbour classification algorithm.
 //You can vary the parameter k for k nearest neighbours to be selected as an estimation.
-//This is binary algorithm set by points' isBlue attribute.
+//This is binary algorithm set by points' is attribute.
 func Knn(k int, p Point, pts []Point) bool {
 	var kValue float64
 	ps := TopKEuclideans(p, pts, k)
 	for _, pt := range ps {
 		var v float64
-		if !pt.isBlue {
+		if !pt.is {
 			v = -1
 		}
 		kValue += Euclidean(p, pt) * v
@@ -1045,4 +1045,210 @@ func swapFs(ps []float64, index int) {
 	val := ps[index]
 	ps[index] = ps[index-1]
 	ps[index-1] = val
+}
+
+//AverageX computes the average of x coordinates of the dataset
+func AverageX(pts Points) float64 {
+	var sum float64
+	for _, p := range pts {
+		sum += p.x
+	}
+	avg := sum / float64(len(pts))
+	return avg
+}
+
+//AverageY computes the average of y coordinates of the dataset
+func AverageY(pts Points) float64 {
+	var sum float64
+	for _, p := range pts {
+		sum += p.y
+	}
+	avg := sum / float64(len(pts))
+	return avg
+}
+
+//VarianceX returns the variance of X coordinates
+func VarianceX(pts Points) float64 {
+	var sum float64
+	avg := AverageX(pts)
+	for _, p := range pts {
+		sum += math.Pow(p.x-avg, 2)
+	}
+	return sum / float64(len(pts))
+}
+
+//VarianceY returns the variance of Y coordinates
+func VarianceY(pts Points) float64 {
+	var sum float64
+	avg := AverageY(pts)
+	for _, p := range pts {
+		sum += math.Pow(p.y-avg, 2)
+	}
+	return sum / float64(len(pts))
+}
+
+//Covariance returns the covariance of a given dataset
+func Covariance(pts Points) float64 {
+	var cov float64
+	avgX := AverageX(pts)
+	avgY := AverageY(pts)
+
+	for _, p := range pts {
+		cov += (p.x - avgX) * (p.y - avgY)
+	}
+	return cov / float64(len(pts))
+}
+
+//StddevX returns the standard devation of X coordinates
+func StddevX(pts Points) float64 {
+	return math.Sqrt(VarianceX(pts))
+}
+
+//StddevY returns the standard devation of Y coordinates
+func StddevY(pts Points) float64 {
+	return math.Sqrt(VarianceY(pts))
+}
+
+//Correlation prints the memo for how the X and Y of the dataset are correlated.
+func Correlation(pts Points) {
+	cov := Covariance(pts)
+	if cov > 0 {
+		fmt.Println("X and Y are positively correlated. ")
+	} else if cov == 0 {
+		fmt.Println("X and Y are not correlated. ")
+	} else {
+		fmt.Println("X and Y are negatively correlated. ")
+	}
+}
+
+//CovarianceMatrix returns the covariance matrix of the dataset.
+func CovarianceMatrix(pts Points) matrix.Matrix {
+	varX := VarianceX(pts)
+	varY := VarianceY(pts)
+	cov := Covariance(pts)
+
+	slc := [][]float64{
+		{varX, cov},
+		{cov, varY},
+	}
+	covMatrix := matrix.NewMatrix(slc)
+	return covMatrix
+}
+
+//DisplayAllStats prints all the statistics considering the dataset.
+func DisplayAllStats(pts Points) {
+	avgX := AverageX(pts)
+	avgY := AverageY(pts)
+	varX := VarianceX(pts)
+	varY := VarianceY(pts)
+	stddevX := StddevX(pts)
+	stddevY := StddevY(pts)
+	cov := Covariance(pts)
+
+	fmt.Printf("Average of X : %.2f\n", avgX)
+	fmt.Printf("Average of Y : %.2f\n", avgY)
+	fmt.Printf("Variance of X : %.2f\n", varX)
+	fmt.Printf("Variance of Y : %.2f\n", varY)
+	fmt.Printf("Standard deviation of X : %.2f\n", stddevX)
+	fmt.Printf("Standard deviation of Y : %.2f\n", stddevY)
+	fmt.Printf("Covariance : %.5f\n", cov)
+
+	CovarianceMatrix(pts)
+	Correlation(pts)
+}
+
+//JaccardIndex returns the Jaccard index.
+func JaccardIndex(p1, p2 Points) int {
+	var sum int
+	for i := range p1 {
+		if (p1[i].is && p2[i].is) || (!p1[i].is && !p2[i].is) {
+			sum++
+		}
+	}
+	return sum / len(p1)
+}
+
+//Evaluation Metrics based on the bool of points, p1 should be the predicted value and p2 the actual values.
+func F1Score(p1, p2 Points) int {
+	return 2 * (Precision(p1, p2) * Recall(p1, p2)) / (Precision(p1, p2) + Recall(p1, p2))
+}
+
+//Sensitivity returns the sensitivity
+func Sensitivity(p1, p2 Points) int {
+	tp := TruePositivies(p1, p2)
+	fn := FalseNegatives(p1, p2)
+	return tp / (tp + fn)
+}
+
+//Specificity returns the specificity
+func Specificity(p1, p2 Points) int {
+	fp := FalsePositives(p1, p2)
+	tn := TrueNegatives(p1, p2)
+	return fp / (fp + tn)
+}
+
+//Precision returns the precision.
+func Precision(p1, p2 Points) int {
+	tp := TruePositivies(p1, p2)
+	fp := FalsePositives(p1, p2)
+	return tp / (tp + fp)
+}
+
+//Recall returns the recall.
+func Recall(p1, p2 Points) int {
+	tp := TruePositivies(p1, p2)
+	fn := FalseNegatives(p1, p2)
+	return tp / (tp + fn)
+}
+
+//TruePositivies returns the number of true positive predicted values.
+func TruePositivies(p1, p2 Points) int {
+	var sum int
+	for i := range p1 {
+		if p1[i].is && p2[i].is {
+			sum++
+		}
+	}
+	return sum
+}
+
+//TrueNegatives returns the number of true negative predicted values.
+func TrueNegatives(p1, p2 Points) int {
+	var sum int
+	for i := range p1 {
+		if !p1[i].is && !p2[i].is {
+			sum++
+		}
+	}
+	return sum
+}
+
+//FalsePositives returns the number of false positive predicted values.
+func FalsePositives(p1, p2 Points) int {
+	var sum int
+	for i := range p1 {
+		if !p1[i].is && p2[i].is {
+			sum++
+		}
+	}
+	return sum
+}
+
+//FalseNegatives returns the number of false negative predicted values.
+func FalseNegatives(p1, p2 Points) int {
+	var sum int
+	for i := range p1 {
+		if p1[i].is && !p2[i].is {
+			sum++
+		}
+	}
+	return sum
+}
+
+//PrintMetrics prints the evaluation metrics.
+func PrintMetrics(p1, p2 Points) {
+	fmt.Printf("Jaccard Index: %d\n", JaccardIndex(p1, p2))
+	fmt.Printf("F1 Score: %d\n", F1Score(p1, p2))
+	fmt.Printf("Precision: %d\n", Precision(p1, p2))
+	fmt.Printf("Recall: %d\n", Recall(p1, p2))
 }
